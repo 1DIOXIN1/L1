032921d76ff989e3b59aeab2ec76ace75b785177
DIOX1N <109106526+1DIOXIN1@users.noreply.github.com>
2026-04-19 21:19:55 +0500

    Add autoLoader

--- Assets/_Project/Develop/Runtime/Utilities/Reactive/IReadOnlyVariable.cs
+++ Assets/_Project/Develop/Runtime/Utilities/Reactive/IReadOnlyVariable.cs
@@ -5,7 +5,7 @@ namespace _Project.Develop.Runtime.Utilities.Reactive
     public interface IReadOnlyVariable<T>
     {
         public IDisposable Subscribe(Action<T, T> action);
-        public T Value{ get;}
+        public T Value{ get; }
     }
 
 
